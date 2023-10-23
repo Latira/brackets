d2de87b1d8ab28440bbb73e3b63056bc9ad590c1
Latira <l@g.com>
2023-10-23 16:11:41 +0300

    init: decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+    let newConfig = [], 
+        j = 0;
+    function checkForType(typeOfBrackets) {
+        if (newConfig[j-1] === typeOfBrackets[0]) {
+            newConfig.splice(j, 1);
+            newConfig.splice(j-1, 1);
+            j = j - 2;
+        }
+    }
+    for (let i = 0; i < str.length; i++) {        
+        newConfig[j] = str[i];
+        console.log(newConfig);
+        for (let c = 0; c < config.length; c++) {
+            cs = `${c}`;
+            if (str[i] === config[cs][1]) {
+                checkForType(config[cs]);
+            }
+        }
+       j++; 
+    }
+    console.log(newConfig);
+    if (newConfig.length == 0) {
+        return true;
+    }  else return false;
 }
